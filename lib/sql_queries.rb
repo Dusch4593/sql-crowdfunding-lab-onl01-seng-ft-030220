e5f134a6a07058c02fe54209dff8a683a464cc26
Brandon Dusch <brandondusch@gmail.com>
2020-04-02 23:51:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, SUM(pledges.amount)
    FROM projects
    JOIN pledges
-   ON projects.id = pledges.project_id 
    ON pledges.amount > projects.funding_goal
    GROUP BY title, funding_goal"
 end
