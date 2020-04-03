34e6b68aff94f3841ca7ddaa4a975fd0dc75f7f0
Brandon Dusch <brandondusch@gmail.com>
2020-04-03 00:14:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,10 +32,10 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT users.name, (SUM(pledges.amount)) AS pledge_total 
+  "SELECT users.name, amount 
    FROM users, pledges 
    GROUP BY name
-   ORDER BY pledge_total;"
+   ORDER BY amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
