54aaf4b005baf9e59ffb56caf111ea42222173a9
Brandon Dusch <brandondusch@gmail.com>
2020-04-03 00:14:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, amount 
-   FROM users, pledges 
+   FROM users 
    GROUP BY name
    ORDER BY amount;"
 end
